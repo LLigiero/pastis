3f6f15550a92a8ce30554f904ec216ee68964877
Youssef El Ouard <youselouard@gmail.com>
2023-06-07 16:26:56 +0200

    Fixed PastisDataset to have 3 datasets

--- pastis/ml_logic/data.py
+++ pastis/ml_logic/data.py
@@ -9,20 +9,33 @@ class PastisDataset:
     Custom Class wrapper to manipulate Tf dataset with S2 data
     """
 
-    def __init__(self, split: str, mono_date: int = 0) -> None:
-        assert split in ["train", "val", "test"]
-
+    def __init__(self, mono_date: int = 0) -> None:
         metadata = load_geojson()
 
-        paths = metadata[metadata.Fold.isin(FOLDS[split])]
-        paths = paths.ID_PATCH.map(
-            lambda id: str(os.path.join(DATA_PATH, "DATA_S2", f"S2_{id}.npy"))
+        splits = ["train", "val", "test"]
+        files_list = {}
+
+        for split in splits:
+            paths = metadata[metadata.Fold.isin(FOLDS[split])]
+            paths = paths.ID_PATCH.map(
+                lambda id: str(os.path.join(DATA_PATH, "DATA_S2", f"S2_{id}.npy"))
+            )
+            files_list[split] = list(paths)
+
+        self.train_dataset = tf.data.Dataset.list_files(files_list["train"])
+        self.train_dataset = self.train_dataset.map(
+            lambda path: tf.py_function(
+                process_path, inp=[path, mono_date], Tout=[tf.float32, tf.uint8]
+            )
         )
-        paths = list(paths)
-
-        self.tf_dataset = tf.data.Dataset.list_files(paths)
-
-        self.tf_dataset = self.tf_dataset.map(
+        self.val_dataset = tf.data.Dataset.list_files(files_list["val"])
+        self.val_dataset = self.val_dataset.map(
+            lambda path: tf.py_function(
+                process_path, inp=[path, mono_date], Tout=[tf.float32, tf.uint8]
+            )
+        )
+        self.test_dataset = tf.data.Dataset.list_files(files_list["val"])
+        self.test_dataset = self.test_dataset.map(
             lambda path: tf.py_function(
                 process_path, inp=[path, mono_date], Tout=[tf.float32, tf.uint8]
             )
