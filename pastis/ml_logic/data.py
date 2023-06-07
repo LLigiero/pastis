6d707fd520cf3eb4a5c2a24739508deaea08ea61
Youssef El Ouard <youselouard@gmail.com>
2023-06-07 16:02:06 +0200

    Added FOLD handling

--- pastis/ml_logic/data.py
+++ pastis/ml_logic/data.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
-from pastis.params import DATA_PATH
-from pastis.ml_logic.utils import process_path
-import pathlib
+from pastis.params import DATA_PATH, FOLDS
+from pastis.ml_logic.utils import process_path, load_geojson
+import os
 
 
 class PastisDataset:
@@ -9,20 +9,21 @@ class PastisDataset:
     Custom Class wrapper to manipulate Tf dataset with S2 data
     """
 
-    def __init__(self, mono_date:int=0) -> None:
+    def __init__(self, split: str, mono_date: int = 0) -> None:
+        assert split in ["train", "val", "test"]
 
-        # self.metadata=load_geojson()
-        # id_patch_list = list(self.metadata['ID_PATCH'])
-        # dates_s2_list = list(self.metadata['dates-S2'].apply(lambda x: list(x.values())))
-        # print (type(dates_s2_list))
-        # print (dates_s2_list)
+        metadata = load_geojson()
 
+        paths = metadata[metadata.Fold.isin(FOLDS[split])]
+        paths = paths.ID_PATCH.map(
+            lambda id: str(os.path.join(DATA_PATH, "DATA_S2", f"S2_{id}.npy"))
+        )
+        paths = list(paths)
 
-        data_path = pathlib.Path(DATA_PATH)
-
-
-        self.tf_dataset = tf.data.Dataset.list_files(str(data_path /'DATA_S2'/ "*"))
+        self.tf_dataset = tf.data.Dataset.list_files(paths)
 
         self.tf_dataset = self.tf_dataset.map(
-            lambda path: tf.py_function(process_path, inp=[path, mono_date], Tout=[tf.float32, tf.uint8])
+            lambda path: tf.py_function(
+                process_path, inp=[path, mono_date], Tout=[tf.float32, tf.uint8]
+            )
         )
