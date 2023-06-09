e5181c7db1c6bb1ad2a9b6966f466675f76f5cd9
Charles deMontulle <charles.demontulle@gmail.com>
2023-06-09 15:31:24 +0200

    updated viz to get info from csv

--- pastis/ml_logic/models/results_viz.py
+++ pastis/ml_logic/models/results_viz.py
@@ -1,6 +1,36 @@
 import matplotlib.pyplot as plt
+import csv
 
-def plot_history(history, title='', axs=None, model_name=""):
+def get_csv_data(file_name):
+    file_name = 'models_output/csv/0.302_baseline_unet_20230609-144414_csvlog.csv'
+
+    d = {'epoch':[],
+        'acc':[],
+        'iou':[],
+        'loss':[],
+        'mean_iou':[],
+        'val_acc':[],
+        'val_iou':[],
+        'val_loss':[],
+        'val_mean_iou':[]}
+
+    with open(file_name, 'r') as csvfile:
+        data = csv.DictReader(csvfile, skipinitialspace=True)
+        for row in data:
+            # row is a dict
+            d['epoch'].append(row['epoch'])
+            d['acc'].append(row['acc'])
+            d['iou'].append(row['iou'])
+            d['loss'].append(row['loss'])
+            d['mean_iou'].append(row['mean_iou'])
+            d['val_acc'].append(row['val_acc'])
+            d['val_iou'].append(row['val_iou'])
+            d['val_loss'].append(row['val_loss'])
+            d['val_mean_iou'].append(row['val_mean_iou'])
+
+    return d
+
+def plot_history(history:dict, title='', axs=None, model_name=""):
     if axs is not None:
         ax1, ax2, ax3 = axs
     else:
@@ -8,20 +38,20 @@ def plot_history(history, title='', axs=None, model_name=""):
 
     if len(model_name) > 0 and model_name[0] != '_':
         model_name = '_' + model_name
-    ax1.plot(history.history['loss'], label = 'train loss' + model_name)
-    ax1.plot(history.history['val_loss'], label = 'val' + model_name)
+    ax1.plot(history['loss'], label = 'train loss' + model_name)
+    ax1.plot(history['val_loss'], label = 'val' + model_name)
     ax1.set_ylim(1, 2.75)
     ax1.set_title('Loss')
     ax1.legend()
 
-    ax2.plot(history.history['acc'], label='train accuracy'  + model_name)
-    ax2.plot(history.history['val_acc'], label='val accuracy'  + model_name)
+    ax2.plot(history['acc'], label='train accuracy'  + model_name)
+    ax2.plot(history['val_acc'], label='val accuracy'  + model_name)
     ax2.set_ylim(0., 1)
     ax2.set_title('Accuracy')
     ax2.legend()
 
-    ax3.plot(history.history['mean_iou'], label='train mean IuO'  + model_name)
-    ax3.plot(history.history['val_mean_iou'], label='val mean IoU'  + model_name)
+    ax3.plot(history['mean_iou'], label='train mean IuO'  + model_name)
+    ax3.plot(history['val_mean_iou'], label='val mean IoU'  + model_name)
     ax3.set_ylim(0., 1)
     ax3.set_title('Mean IuO')
     ax3.legend()
