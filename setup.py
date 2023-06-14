022ff8d1bd674d6ed6c98cdc26d68a9add0c8f64
Leticia Ligiero <leticia.ligiero@totalenergies.com>
2023-06-14 15:39:05 +0200

    Added README info and legend style

--- setup.py
+++ setup.py
@@ -7,15 +7,14 @@ requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(
     name="pastis",
-    version="0.0.1",
+    version="0.0.2",
     description="Pastis package",
     license="MIT",
-    # author="Le Wagon",
-    # author_email="contact@lewagon.org",
-    # url="https://github.com/lewagon/taxi-fare",
+    # author="",
+    # author_email="",
+    # url="",
     install_requires=requirements,
     packages=find_packages(),
-    # test_suite="tests",
     # include_package_data: to install data from MANIFEST.in
     include_package_data=True,
     zip_safe=False,
